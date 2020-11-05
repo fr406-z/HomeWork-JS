a10871d9100f6d3de4a4b2a200168bd6adc6602e
Aleks <73532802+fr406-z@users.noreply.github.com>
2020-11-05 13:17:18 +0200

    Add files via upload

---
+++ HW_1/js/script.js
@@ -0,0 +1,73 @@
+/* 2 Задание */
+
+let aa1 = 5 % 3;
+    aa2 = 3 % 5;
+    aa3 = 5 + '3';
+    aa4 = '5' - 3;
+    aa5 = 75 + 'кг';
+    aa6 = 785 * 653;
+    aa7 = 100 / 25;
+    aa8 = 0 * 0;
+    aa9 = 0 / 2;
+    aa10 = 89 / 0;
+    aa11 = 98 + 2;
+    aa12 = 5 - 98;
+    aa13 = (8 + 56 * 4)/5;
+    aa14 = (9 - 12)*7/(5 + 2);
+    aa15 = +"123";
+    aa16 = 1 || 0;
+    aa17 = false || true;
+    aa18 = true>0;
+
+console.log(aa1, aa2, aa3, aa4, aa5, aa6, aa7, aa8, aa9, aa10, aa11, aa12, aa13, aa14, aa15, aa16, aa17, aa18);
+
+
+/* 3 задание */
+
+let height=23, width = 10;
+let SPryam = height * width;
+console.log(SPryam + "см.кв.");
+
+/* 4 задание */
+
+let Vheight = 10,
+Vr = aa7/2;
+let VCilindra = Vr * Vr * Vheight * 3.14;
+console.log(VCilindra + "см.куб");
+
+/* 5 задание */
+
+let r = 5;
+let Skruga = r * r * 3.14;
+console.log(Skruga + "см.кв.");
+
+/* 6 задание */
+
+let ar = 5, br = 7, h = 10 ;
+let STrap = ar * br * h;
+console.log(STrap + "см.куб");
+
+/* 7 задание */
+
+let S = 2000000 , p = 0.1 , years = 5;
+let Pereplata = S * p * years;
+console.log(Pereplata);
+
+/* 8 задание */
+
+let a = 8, b = 3;
+
+//a + 2 * (x - b) = 16
+//x = (16 - a) / 2 + b;
+
+//b (x + 15) = a + 6*x;
+//x = (b * 15 - a)/(6 - b);
+
+//x + 2 * x + a * x + b * x = 23780;
+x = 23780 / (1 + 2 + a + b); 
+
+console.log(x);
+
+
+/* 9 задание */
+console.log (' Бывало, спит у ног собака, \n костер занявшийся гудит,\n и женщина из полумрака \n глазами зыбкими глядит.\n \n \n Потом под пихтою приляжет \n на куртку рыжую мою \n и мне, задумчивая, скажет:\n \n \n "А ну-ка, спой!.."- и я пою.');
